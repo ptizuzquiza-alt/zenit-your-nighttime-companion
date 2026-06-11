a7edc02ba5b36c05a15fd959f91c509b36ac506c
ptizuzquiza-alt <ptizuzquiza@gmail.com>
2026-06-11 22:43:04 +0200

    k

--- src/pages/Friends.tsx
+++ src/pages/Friends.tsx
@@ -407,7 +407,7 @@ const Friends: FC = () => {
       </div>
 
       {/* Scrollable content */}
-      <div className="flex-1 overflow-y-auto px-4 pb-24 space-y-5">
+      <div className="flex-1 min-h-0 overflow-y-auto px-4 pb-24 space-y-5">
 
         {/* Share profile */}
         <button
